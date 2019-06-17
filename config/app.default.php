b78917eb0e8a5c45621d21f5055fcd51129f35c1
Paulo Henrique <40889759+paulohlimamaia@users.noreply.github.com>
2019-06-17 08:58:37 -0300

    Update app.default.php

--- config/app.default.php
+++ config/app.default.php
@@ -248,7 +248,7 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Postgres',
             'persistent' => false,
-            'host' => 'ec2-54-83-9-169.compute-1.amazonaws.com',
+            'host' => 'localhost',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
@@ -296,7 +296,7 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Postgres',
             'persistent' => false,
-            'host' => 'ec2-54-83-9-169.compute-1.amazonaws.com',
+            'host' => 'localhost',
             //'port' => 'non_standard_port_number',
             'username' => 'postgres',
             'password' => 'senha',
